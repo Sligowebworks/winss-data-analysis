ceb204718f8ae0d675ac7b3784ccd093f8b648a7
Michael Z. Daryabeygi <mzd@ginkgostreet.com>
2012-03-15 05:11:39 +0000

    increment wsas year;

--- SligoCS.Web.WI/StateTestsPerformance.aspx.cs
+++ SligoCS.Web.WI/StateTestsPerformance.aspx.cs
@@ -73,7 +73,7 @@ namespace SligoCS.Web.WI
         protected override void OnInitComplete(EventArgs e)
         {
             GlobalValues.TrendStartYear = (GlobalValues.WOW.Key == WOWKeys.WKCE) ? 1997 : 2003;
-            GlobalValues.CurrentYear = 2011;
+            GlobalValues.CurrentYear = 2012;
 
             QueryMarshaller.RaceDisagCodes.Add((int)QueryMarshaller.RaceCodes.RaceEth_NA);
 
