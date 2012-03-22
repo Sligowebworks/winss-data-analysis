e9701f7dd9f51db23916f868fe9687ecba2f45b5
Michael Z. Daryabeygi <mzd@ginkgostreet.com>
2012-03-22 15:09:57 +0000

    increment activities year;

--- SligoCS.Web.WI/ActivitiesParticipate.aspx.cs
+++ SligoCS.Web.WI/ActivitiesParticipate.aspx.cs
@@ -47,7 +47,7 @@ namespace SligoCS.Web.WI
             GlobalValues.Group.Value = GlobalValues.Group.Range[GroupKeys.All];
 
             GlobalValues.TrendStartYear = 1997;
-            GlobalValues.CurrentYear = 2010;
+            GlobalValues.CurrentYear = 2011;
 
             if (GlobalValues.Show.Key == ShowKeys.Community)
             {
