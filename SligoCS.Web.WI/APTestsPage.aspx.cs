ed8942add80e7d7df93f5e7f101a9f9717828ccb
Michael Z. Daryabeygi <mzd@ginkgostreet.com>
2012-12-14 14:04:02 +0000

    AP year increment;

--- SligoCS.Web.WI/APTestsPage.aspx.cs
+++ SligoCS.Web.WI/APTestsPage.aspx.cs
@@ -47,7 +47,7 @@ namespace SligoCS.Web.WI
             GlobalValues.Grade.Key = GradeKeys.Grades_9_12_Combined;
 
             GlobalValues.TrendStartYear = 1997;
-            GlobalValues.CurrentYear = 2011;
+            GlobalValues.CurrentYear = 2012;
 
             if (UserValues.OrgLevel.Key == OrgLevelKeys.School) 
                 GlobalValues.OrgLevel.Value = GlobalValues.OrgLevel.Range[OrgLevelKeys.District];
