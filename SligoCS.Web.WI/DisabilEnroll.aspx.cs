03f8fb1bf5947eb1948596d4b372128c778ad9e5
Michael Z. Daryabeygi <mzd@ginkgostreet.com>
2014-04-14 23:13:12 +0000

    year increment for Primary Disabilities

--- SligoCS.Web.WI/DisabilEnroll.aspx.cs
+++ SligoCS.Web.WI/DisabilEnroll.aspx.cs
@@ -66,7 +66,7 @@ namespace SligoCS.Web.WI
         protected override void OnInitComplete(EventArgs e)
         {
             GlobalValues.TrendStartYear = 2003;
-            GlobalValues.CurrentYear = 2013;
+            GlobalValues.CurrentYear = 2014;
 
             DisabilitiesDataGrid.ColumnLoaded += new EventHandler(DisabilitiesDataGrid_ColumnLoaded);
 
