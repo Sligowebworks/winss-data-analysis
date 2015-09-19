65558f6e2e3952deb816c60a7a516e3692ac9114
Michael Z. Daryabeygi <mzd@ginkgostreet.com>
2015-09-19 11:41:49 +0000

    mv 2010.08.05/ to file:///var/svn/repo0/wi_branches/tags/stage_2010.08.05/

---
+++ SligoCS.Web.WI/DAL/DALAttendance.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SligoCS.DAL.WI
+{
+    public class DALAttendance : DALWIBase
+    {
+
+        /*
+        public v_AttendanceWWoDisSchoolDistStateEconELP GetAttendanceData(List<int> raceCode,
+            List<int> sexCode,
+            List<int> disabilityCode,
+            List<int> year,
+            List<string> fullKey,
+            List<int> gradeCodeRange,
+            List<int> schoolType,
+            List<int> econDisadv,
+            List<int> ELPCode,
+            List<string> orderBy)*/
+        public override string  BuildSQL(SligoCS.BL.WI.QueryMarshaller Marshaller)
+        {
+           // v_AttendanceWWoDisSchoolDistStateEconELP ds = new v_AttendanceWWoDisSchoolDistStateEconELP();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("select * from v_AttendanceWWoDisSchoolDistStateEconELP where ");
+
+            sb.Append(SQLHelper.WhereClauseValuesInList(SQLHelper.WhereClauseJoiner.NONE, "SchoolType", Marshaller.stypList));
+
+            //Adds " ... AND (SexCode in (1, 2)) ..."
+            sb.Append(SQLHelper.WhereClauseValuesInList(SQLHelper.WhereClauseJoiner.AND, "SexCode", Marshaller.sexCodes));
+
+            ////Adds " ... AND (RaceCode in (1, 2, 3, 4, 5)) ..."
+            sb.Append(SQLHelper.WhereClauseValuesInList(SQLHelper.WhereClauseJoiner.AND, "RaceCode", Marshaller.raceCodes));
+
+            ////Adds " ... AND ((GradeCode >= 16) AND (GradeCode <= 64)) ..."
+            sb.Append(SQLHelper.WhereClauseSingleValueOrInclusiveRange(SQLHelper.WhereClauseJoiner.AND, "GradeCode", Marshaller.gradeCodes));
+
+            //Adds " ... AND (DisabilityCode in (1, 2)) ..."
+            sb.Append(SQLHelper.WhereClauseValuesInList(SQLHelper.WhereClauseJoiner.AND, "DisabilityCode", Marshaller.disabilityCodes));
+
+            //Adds " ... AND (EconDisadvCode in (1, 2)) ..."
+            sb.Append(SQLHelper.WhereClauseValuesInList(SQLHelper.WhereClauseJoiner.AND, "EconDisadvCode", Marshaller.econDisadvCodes));
+
+            //Adds " ... AND (ELPCode in (1, 2)) ..."
+            sb.Append(SQLHelper.WhereClauseValuesInList(SQLHelper.WhereClauseJoiner.AND, "ELPCode", Marshaller.ELPCodes));
+
+            //Adds " ... AND ((year >= 1997) AND (year <= 2007)) ..."
+            sb.Append(SQLHelper.WhereClauseSingleValueOrInclusiveRange(SQLHelper.WhereClauseJoiner.AND, "year", Marshaller.years));
+
+            //fullkey
+            if (!Marshaller.compareSelectedFullKeys)
+            {
+                sb.Append(SQLHelper.WhereClauseValuesInList(
+                    SQLHelper.WhereClauseJoiner.AND, "FullKey", Marshaller.fullkeylist));
+            }
+            else
+            {
+                sb.Append(" and ").Append(Marshaller.clauseForCompareSelected);
+            }
+
+            //order by clause
+            //sb.AppendFormat(" ORDER BY {0}", SQLHelper.ConvertToCSV(orderBy, false));
+            sb.Append(SQLHelper.GetOrderByClause(Marshaller.orderByList));
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ SligoCS.Web.WI/DAL/DALCourseOfferings.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using SligoCS.Web.WI.WebSupportingClasses.WI;
+
+namespace SligoCS.DAL.WI
+{
+    public class DALCourseOfferings : DALWIBase
+    {
+        /*public v_Course_Offerings GetData(
+            List<string> fullKey,
+            List<int> year,
+            CourseTypeID CourseTypeID,
+            WMAS WMASID1,
+            string clauseForCompareToSchoolsDistrict,
+            bool useFullkeys,
+            List<string> orderBy)*/
+        public override string  BuildSQL(SligoCS.BL.WI.QueryMarshaller Marshaller)
+        {
+            StringBuilder sql = new StringBuilder();
+
+            sql.Append("SELECT * FROM v_course_offerings WHERE ");
+
+            sql.Append(SQLHelper.WhereClauseSingleValueOrInclusiveRange(SQLHelper.WhereClauseJoiner.NONE, "year", Marshaller.years));
+
+            sql.Append(SQLHelper.WhereClauseEquals(SQLHelper.WhereClauseJoiner.AND, "CourseTypeID", Marshaller.GlobalValues.CourseTypeID.Value));
+
+            sql.Append(SQLHelper.WhereClauseEquals(SQLHelper.WhereClauseJoiner.AND, "WMASID1", Marshaller.GlobalValues.WMAS.Value));
+
+            //fullkey
+            if (Marshaller.compareSelectedFullKeys)
+            {
+                sql.Append(SQLHelper.WhereClauseJoiner.AND).Append(Marshaller.clauseForCompareSelected);
+            }
+            else
+            {
+                sql.Append(SQLHelper.WhereClauseValuesInList(
+                    SQLHelper.WhereClauseJoiner.AND, "FullKey", Marshaller.fullkeylist));
+            }
+            
+            // jdj: must exclude null district records - workaround - databse object design is messy and needs refactoring
+            sql.Append(" and linkeddistrictname IS NOT NULL ");
+
+           // jdj: must exclude topic code 999 when selecting Subject: Other - workaround - databse object design is messy and needs refactoring
+            if (Marshaller.GlobalValues.WMAS.Key == WMASKeys.Other)  sql.Append(" and v_course_offerings.topic <> '999'  ");
+
+            sql.Append(SQLHelper.GetOrderByClause(Marshaller.orderByList));
+
+            return sql.ToString();
+        }
+    }
+}
