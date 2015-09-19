89865b17c3d97337576e8e02606319ae5217b30e
Michael Z. Daryabeygi <mzd@ginkgostreet.com>
2015-09-19 11:42:21 +0000

    mv 03.28b/ to file:///var/svn/repo0/wi_branches/tags/stage_2011.03.28b/

---
+++ SligoCS.Web.WI/js/trans_popup.js
@@ -0,0 +1,7 @@
+function popup(url) {
+    window.open(url,"_blank",'toolbar=1,location=1,directories=0,status=1,menubar=1,scrollbars=1,resizable=yes,width=500,height=300');
+	}
+	
+function transparencypopup(url) {
+	window.open(url,"_blank",'toolbar=1,location=1,directories=0,status=1,menubar=1,scrollbars=1,resizable=yes,width=800,height=400');
+	}
