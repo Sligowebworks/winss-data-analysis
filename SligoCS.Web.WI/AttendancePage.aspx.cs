a32b8cd72e9df6a307299c597651060b128ef686
Michael Z. Daryabeygi <mzd@ginkgostreet.com>
2015-09-19 11:25:16 +0000

    mv 2011.04.04/ to tags/production_2011.04.04/

---
+++ SligoCS.Web.WI/AttendancePage.aspx.cs
@@ -0,0 +1,134 @@
+using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+
+using SligoCS.DAL.WI;
+using SligoCS.BL.WI;
+using SligoCS.Web.Base.PageBase.WI;
+
+using SligoCS.Web.WI.WebSupportingClasses.WI;
+using SligoCS.Web.WI.WebUserControls;
+
+namespace SligoCS.Web.WI
+{
+
+    public partial class AttendancePage : PageBaseWI
+    {
+        //protected v_AttendanceWWoDisSchoolDistStateEconELP _ds = null;
+        protected DataSet _ds;
+        
+        protected override DataSet InitDataSet()
+        {
+            //_ds = new v_AttendanceWWoDisSchoolDistStateEconELP();
+            _ds = new DataSet();
+            return _ds;
+        }
+        protected override DALWIBase InitDatabase()
+        {
+            return new DALAttendance();
+        }
+        protected override GridView InitDataGrid()
+        {
+            return AttendanceDataGrid;
+        }
+        protected override ChartFX.WebForms.Chart InitGraph()
+        {
+            return barChart;
+        }
+        protected override NavViewByGroup InitNavRowGroups()
+        {
+            nlrVwBy.LinksEnabled = NavViewByGroup.EnableLinksVector.EconElp;
+
+            return nlrVwBy;
+        }
+        protected override string SetPageHeading()
+        {
+            return "What percent of students attend school each day?";
+        }
+        protected override void OnInitComplete(EventArgs e)
+        {            
+            GlobalValues.Grade.Key = GradeKeys.Combined_PreK_12;
+            GlobalValues.Year = 2009;
+
+            if (GlobalValues.Group.Key == GroupKeys.Disability)
+            {
+                GlobalValues.TrendStartYear = 2005;
+            }
+            else if (GlobalValues.Group.Key == GroupKeys.EngLangProf
+                    || GlobalValues.Group.Key == GroupKeys.EconDisadv)
+            {
+                GlobalValues.TrendStartYear = 2005;
+            }
+            else
+            {
+                GlobalValues.TrendStartYear = 1997;
+            }
+            base.OnInitComplete(e);
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            set_visible();
+            DataSetTitle = GetTitle("Attendance Rate");
+            AttendanceDataGrid.AddSuperHeader(DataSetTitle);
+
+            SetUpChart(_ds);
+        }
+        private void SetUpChart(DataSet _ds)
+        {
+            try
+            {
+                barChart.AxisY.LabelsFormat.Decimals = 0;
+                barChart.AxisY.DataFormat.Decimals = 1;
+                barChart.AxisY.LabelsFormat.Format = ChartFX.WebForms.AxisFormat.Percentage;
+                barChart.AxisY.DataFormat.Format = ChartFX.WebForms.AxisFormat.Percentage;
+                barChart.AxisY.ScaleUnit = 100;
+                barChart.AxisY.Step = 10;
+
+                //barChart.LegendBox.ContentLayout = ChartFX.WebForms.ContentLayout.Center;
+                //barChart.LegendBox.PlotAreaOnly = false;
+
+                //if (GlobalValues.STYP.Key == STYPKeys.Elem)
+                 if (GlobalValues.Group.Key == GroupKeys.All)
+                {
+                    List<String> friendlyAxisXName = new List<String>();
+                    friendlyAxisXName.Add("All Students");
+                    barChart.FriendlyAxisXNames = friendlyAxisXName;
+                }
+                barChart.Title = DataSetTitle;
+                barChart.AxisYDescription = "Attendance Rate";
+                barChart.DisplayColumnName = "Attendance Rate";
+                //barChart.Data[0, i] = Convert.ToDouble(_ds.Tables[0].Rows[i][v_HSCWWoDisSchoolDistStateEconELP.Regular_Diplomas].ToString());
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+            }
+        }
+
+        private void set_visible()
+        {
+            ((SligoCS.Web.WI.WI)Page.Master).set_visible_state(WI.displayed_obj.Mixed_Header_Graphics1, true);
+            ((SligoCS.Web.WI.WI)Page.Master).set_visible_state(WI.displayed_obj.dataLinksPanel, true);
+        }
+
+        public override List<string> GetVisibleColumns(Group viewBy, OrgLevel orgLevel, CompareTo compareTo, STYP schoolType)
+        {
+            List<string> retval =  base.GetVisibleColumns(viewBy, orgLevel, compareTo, schoolType);
+            retval.Add("Enrollment PreK-12");
+            retval.Add("Actual Days Of Attendance");
+            retval.Add("Possible Days Of Attendance");
+            retval.Add("Attendance Rate");
+
+            return retval;
+        }
+    }
+}
---
+++ SligoCS.Web.WI/DAL/DALSuspExpIncidents.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SligoCS.DAL.WI
+{
+    public class DALSuspExpIncidents : DALWIBase
+    {
+        public override string  BuildSQL(SligoCS.BL.WI.QueryMarshaller Marshaller)
+        {
+            StringBuilder sql = new StringBuilder();
+            sql.Append("SELECT * FROM v_SuspExpIncidentsWWoDisSchoolDistState WHERE");
+
+            sql.Append(SQLHelper.WhereClauseValuesInList(SQLHelper.WhereClauseJoiner.NONE, "SchoolType", Marshaller.stypList));
+
+            ////Adds " ... AND (SexCode in (1, 2)) ..."
+            sql.Append(SQLHelper.WhereClauseValuesInList(SQLHelper.WhereClauseJoiner.AND, "SexCode", Marshaller.sexCodes));
+
+            //Adds " ... AND (RaceCode in (1, 2, 3, 4, 5)) ..."
+            sql.Append(SQLHelper.WhereClauseValuesInList(SQLHelper.WhereClauseJoiner.AND, "RaceCode", Marshaller.raceCodes));
+
+            //Adds " ... AND ((GradeCode >= 16) AND (GradeCode <= 64)) ..."
+            sql.Append(SQLHelper.WhereClauseSingleValueOrInclusiveRange(SQLHelper.WhereClauseJoiner.AND, "GradeCode", Marshaller.gradeCodes));
+
+            //Adds " ... AND (DisabilityCode in (1, 2)) ..."
+            sql.Append(SQLHelper.WhereClauseValuesInList(SQLHelper.WhereClauseJoiner.AND, "DisabilityCode", Marshaller.disabilityCodes));
+
+            //Adds " ... AND (EconDisadvCode in (1, 2)) ..."
+            //sql.Append(SQLHelper.WhereClauseValuesInList(SQLHelper.WhereClauseJoiner.AND, "EconDisadvCode", Marshaller.Ec));
+
+            //Adds " ... AND (ELPCode in (1, 2)) ..."
+            //sql.Append(SQLHelper.WhereClauseValuesInList(SQLHelper.WhereClauseJoiner.AND, "ELPCode", Marshaller. ELP));
+
+            //Adds " ... AND ((year >= 1997) AND (year <= 2007)) ..."
+            sql.Append(SQLHelper.WhereClauseSingleValueOrInclusiveRange(SQLHelper.WhereClauseJoiner.AND, "year", Marshaller.years));
+
+            //fullkey
+            if (Marshaller.compareSelectedFullKeys)
+            {
+                sql.Append(" and ").Append(Marshaller.clauseForCompareSelected);
+            }
+            else
+            {
+                sql.Append(SQLHelper.WhereClauseValuesInList(SQLHelper.WhereClauseJoiner.AND, "FullKey", Marshaller.fullkeylist));
+            }
+
+            sql.AppendFormat(SQLHelper.GetOrderByClause(Marshaller.orderByList));
+
+            return sql.ToString();
+        }
+    }
+}
