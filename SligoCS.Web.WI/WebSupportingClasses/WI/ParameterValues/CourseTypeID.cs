8efabdfc7832d9f3d4cfd31079b227b2946e7878
Michael Z. Daryabeygi <mzd@ginkgostreet.com>
2015-09-19 11:41:50 +0000

    mv 2010.12.14/ to file:///var/svn/repo0/wi_branches/tags/stage_2010.12.14/

---
+++ SligoCS.Web.WI/WebSupportingClasses/WI/ParameterValues/CourseTypeID.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SligoCS.Web.WI.WebSupportingClasses.WI
+{
+	public struct CourseTypeIDKeys
+	{
+		public const string AP = "Advanced Placement Program&reg;";
+		public const string CAPP = "CAPP";
+		public const string IB = "International Baccalaureate";
+		public const string Other = "Other Courses";
+	}
+
+	public class CourseTypeID : ParameterValues
+	{
+		private static SerializableDictionary<String, String> range;
+
+		public override SerializableDictionary<string, string> Range
+		{
+			get
+			{
+				if (range == null) range = initRange();
+				return range;
+			}
+		}
+		protected  SerializableDictionary<String, String> initRange()
+		{
+			SerializableDictionary<String, String> range = new SerializableDictionary<String, String>();
+
+			range.Add(CourseTypeIDKeys.AP, "1");
+			range.Add(CourseTypeIDKeys.CAPP, "2");
+			range.Add(CourseTypeIDKeys.IB, "6");
+			range.Add(CourseTypeIDKeys.Other, "4");
+
+			return range;
+		}
+	}
+}
