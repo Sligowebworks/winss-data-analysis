5d4bb643f47e8e5863d46df6da0cc3c1482e9bc0
Michael Z. Daryabeygi <mzd@ginkgostreet.com>
2015-09-19 11:25:16 +0000

    mv 2010.11.02/ to tags/production_2010.11.02/

---
+++ SligoCS.Web.WI/DAL/DALRetention.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SligoCS.DAL.WI
+{
+    public class DALRetention : DALWIBase
+    {
+    public override string  BuildSQL(SligoCS.BL.WI.QueryMarshaller Marshaller)
+    {
+            StringBuilder sql = new StringBuilder();
+            sql.Append("select * from v_RetentionWWoDisEconELPSchoolDistState where ");
+
+            sql.Append(SQLHelper.WhereClauseValuesInList(SQLHelper.WhereClauseJoiner.NONE, "SchoolType", Marshaller.stypList));
+
+            //Adds " ... AND (SexCode in (1, 2)) ..."
+            sql.Append(SQLHelper.WhereClauseValuesInList(SQLHelper.WhereClauseJoiner.AND, "SexCode", Marshaller.sexCodes));
+
+            //Adds " ... AND (RaceCode in (1, 2, 3, 4, 5)) ..."
+            sql.Append(SQLHelper.WhereClauseValuesInList(SQLHelper.WhereClauseJoiner.AND, "RaceCode", Marshaller.raceCodes));
+
+            //Adds " ... AND ((GradeCode >= 16) AND (GradeCode <= 64)) ..."
+            sql.Append(SQLHelper.WhereClauseSingleValueOrInclusiveRange(SQLHelper.WhereClauseJoiner.AND, "GradeCode", Marshaller.gradeCodes));
+
+            //Adds " ... AND (DisabilityCode in (1, 2)) ..."
+            sql.Append(SQLHelper.WhereClauseValuesInList(SQLHelper.WhereClauseJoiner.AND, "DisabilityCode", Marshaller.disabilityCodes));
+
+            //BR 
+            //Adds " ... AND (EconDisadvCode in (1, 2)) ..."
+            sql.Append(SQLHelper.WhereClauseValuesInList(SQLHelper.WhereClauseJoiner.AND, "EconDisadvCode", Marshaller.econDisadvCodes));
+
+            //Adds " ... AND (ELPCode in (1, 2)) ..."
+            sql.Append(SQLHelper.WhereClauseValuesInList(SQLHelper.WhereClauseJoiner.AND, "ELPCode", Marshaller.ELPCodes));
+
+
+            //Adds " ... AND ((year >= 1997) AND (year <= 2007)) ..."
+            sql.Append(SQLHelper.WhereClauseSingleValueOrInclusiveRange(SQLHelper.WhereClauseJoiner.AND, "year", Marshaller.years));
+        
+            if (Marshaller.compareSelectedFullKeys)
+            {
+                sql.Append(" and ").Append( Marshaller.clauseForCompareSelected);
+            }
+            else
+            {
+                sql.Append(SQLHelper.WhereClauseValuesInList(
+                    SQLHelper.WhereClauseJoiner.AND, "FullKey", Marshaller.fullkeylist));
+            }
+            
+
+            //order by clause
+            //sql.AppendFormat(" ORDER BY {0}", SQLHelper.ConvertToCSV(orderBy, false));
+            sql.Append(SQLHelper.GetOrderByClause(Marshaller.orderByList));
+
+            return sql.ToString();
+        }
+    }
+}
