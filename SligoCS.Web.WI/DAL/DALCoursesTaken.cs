d525a440c2a130a4e699a2d1cd0232f2b112803d
Michael Z. Daryabeygi <mzd@ginkgostreet.com>
2015-09-19 11:42:20 +0000

    mv 03.22a/ to file:///var/svn/repo0/wi_branches/tags/stage_2011.03.22a/

---
+++ SligoCS.Web.WI/DAL/DALCoursesTaken.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using SligoCS.Web.WI.WebSupportingClasses.WI;
+
+namespace SligoCS.DAL.WI
+{
+    public class DALCoursesTaken : DALWIBase
+    {
+        public override string  BuildSQL(SligoCS.BL.WI.QueryMarshaller Marshaller)
+        {
+            StringBuilder sql = new StringBuilder();
+
+            sql.Append("SELECT * FROM v_Coursework WHERE ");
+
+            sql.Append(SQLHelper.WhereClauseSingleValueOrInclusiveRange(SQLHelper.WhereClauseJoiner.NONE, "year", Marshaller.years));
+
+            //fullkey
+            if (Marshaller.compareSelectedFullKeys)
+            {
+                sql.Append(SQLHelper.WhereClauseJoiner.AND).Append(Marshaller.clauseForCompareSelected);
+            }
+            else
+            {
+                sql.Append(SQLHelper.WhereClauseValuesInList(SQLHelper.WhereClauseJoiner.AND, "FullKey", Marshaller.fullkeylist));
+            }
+
+            //Adds " ... AND (SexCode in (1, 2)) ..."
+            sql.Append(SQLHelper.WhereClauseValuesInList(SQLHelper.WhereClauseJoiner.AND, "Sex", Marshaller.sexCodes));
+
+            sql.Append(SQLHelper.WhereClauseValuesInList(SQLHelper.WhereClauseJoiner.AND, "Grade", Marshaller.gradeCodes));
+
+            sql.Append(SQLHelper.WhereClauseEquals(SQLHelper.WhereClauseJoiner.AND, "CourseTypeID", Marshaller.GlobalValues.CourseTypeID.Value));
+
+            sql.Append(SQLHelper.WhereClauseEquals(SQLHelper.WhereClauseJoiner.AND, "WMASID1", Marshaller.GlobalValues.WMAS.Value));
+
+            sql.Append(SQLHelper.GetOrderByClause(Marshaller.orderByList));
+
+            return sql.ToString();
+        }
+    }
+}
---
+++ SligoCS.Web.WI/WebUserControls/AlphaLinkWebUserControl.ascx
@@ -0,0 +1,5 @@
+<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="AlphaLinkWebUserControl.ascx.cs" Inherits="SligoCS.Web.WI.WebUserControl1" %>
+<br />
+<asp:PlaceHolder ID="PlaceHolder1" runat="server"></asp:PlaceHolder>
+<br />
+
